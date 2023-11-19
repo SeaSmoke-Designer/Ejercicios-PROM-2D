eb459f38849443bcbfa362af99dd5584b7e8f038
SeaSm0kee <iantaulam@alu.edu.gva.es>
2023-11-19 20:05:35 +0100

    Cambios AudioManager

--- FlappyBird/Assets/Scripts/AudioManager.cs
+++ FlappyBird/Assets/Scripts/AudioManager.cs
@@ -2,28 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
     [SerializeField]
     private AudioSource sfxAudioSource, musicAudioSource;
 
-    public static AudioManager Instance { get; private set; }
-
-    void Awake()
-    {
-        if (Instance != null && Instance != null)
-            Destroy(this);
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
-
-    }
-
     public void PlayClip(AudioClip clip)
     {
         sfxAudioSource.PlayOneShot(clip);
+        //GetComponent<AudioSource>().PlayOneShot(clip);
     }
 
 }
